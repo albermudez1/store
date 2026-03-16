ac669ba19987070fa4930961f0b5ab910f66cd42
albermudez1 <albermudezmu@unal.edu.co>
2026-03-15 21:08:35 -0500

    feat: integrate inventory operations into Laravel gateway

--- services/gateway-laravel/routes/api.php
+++ services/gateway-laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\ProductGatewayController;
 
 Route::post('/register', [AuthController::class, 'register']);
 
@@ -12,5 +13,14 @@ Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::middleware('auth:api')->group(function () {
     Route::get('/me', [AuthController::class, 'me']);
     Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/products', [ProductGatewayController::class, 'index']);
+    Route::post('/products', [ProductGatewayController::class, 'store']);
+    Route::get('/products/{id}', [ProductGatewayController::class, 'show']);
+    Route::put('/products/{id}', [ProductGatewayController::class, 'update']);
+    Route::delete('/products/{id}', [ProductGatewayController::class, 'destroy']);
+    Route::get('/products/{id}/stock', [ProductGatewayController::class, 'stock']);
+    Route::patch('/products/{id}/stock', [ProductGatewayController::class, 'decreaseStock']);
+    
 });
 
